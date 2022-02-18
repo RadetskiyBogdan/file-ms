06ad468f0e8080d4a6a02494bf7fd97e9ae9dc18
Vladimir <valodya2008@gmail.com>
2022-02-18 12:31:27 +0200

    added developers module

--- file-ms/src/config/configuration.ts
+++ file-ms/src/config/configuration.ts
@@ -12,7 +12,7 @@ export const configuration = {
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        entities: [__dirname + '/../**/*.entity{.ts}'],
+        entities: ['dist/**/*.entity.{ts,js}'],
         synchronize: false
     }
 };
